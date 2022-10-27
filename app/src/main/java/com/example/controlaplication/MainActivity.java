7aa5edb189b396f5c0dcea981375d7a84f7692ae
Ignis010 <HimeraGriff090080007>
2022-10-27 15:01:35 +0900

    Initial commit

---
+++ app/src/main/java/com/example/controlaplication/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.controlaplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void Click(View view) {
+        Intent table = new Intent(this, table.class);
+        startActivity(table);
+
+    }
+}
